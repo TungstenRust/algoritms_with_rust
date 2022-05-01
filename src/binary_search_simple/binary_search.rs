f41105b4940812e335f927801b070e363fe23eda
Tungsten Rust <tungsten.rust@gmail.com>
2022-05-01 05:52:49 -0700

    binary search all variations are added

---
+++ src/binary_search_simple/binary_search.rs
@@ -0,0 +1,19 @@
+pub fn find_via_binary_search(array: &[i32], key: i32) -> Option<usize> {
+    if array.len() == 0 {
+       None
+    } else {
+        let mut left_point = 0;
+        let mut right_point = array.len();
+        while left_point < right_point {
+            let mid_point = (left_point + right_point) / 2;
+            if array[mid_point] == key {
+                return Some(mid_point);
+            } else if array[mid_point] < key {
+                left_point = mid_point + 1;
+            } else if array[mid_point] > key {
+                right_point = mid_point;
+            }
+        }
+        None
+    }
+}
