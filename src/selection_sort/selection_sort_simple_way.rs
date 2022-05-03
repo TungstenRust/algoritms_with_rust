fd16cf0f588c5f029ddf771290632b6f4b2374e3
Tungsten Rust <tungsten.rust@gmail.com>
2022-05-03 08:16:34 -0700

    selection sort simple and easy way added

---
+++ src/selection_sort/selection_sort_simple_way.rs
@@ -0,0 +1,21 @@
+fn main(){
+    let mut list_nums[i32; 7]=[10, 23, 4, 5, 66, 7, -3];
+    println!(":?", list_nums);
+
+    let list_len = list_nums.len();
+    for i in 0..list_len {
+        for j in i+1..list_len {
+            if list_nums[j] < list_nums[i] {
+                swap_us(&mut list_nums, j, i);
+            }
+            println!("{:?}", list_nums);
+        }
+    }
+}
+
+fn swap_us(m_list: &mut [i32;7], j:usize, i:usize){
+    let temp:i32;
+    temp=m_list[i];
+    m_list[i] = m_list[j];
+    m_list[j] = temp;
+}
